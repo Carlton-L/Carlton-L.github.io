11548c91d747210c8cb7a89e4b6f6a442725e227
Carlton <Carlton.l@outlook.com>
2020-05-06 19:57:06 -0700

    Created color scrolling effect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <section
+                data-background-color='rgb(81, 24, 69)'
+                className='js-color-stop'
+                style={{height: '100vh'}}
+            >
+                {/* content */}
+            </section>
+            <section
+                data-background-color='rgb(144, 12, 63)'
+                className='js-color-stop'
+                style={{height: '100vh'}}
+            >
+                {/* content */}
+            </section>
+            <section
+                data-background-color='rgb(199, 0, 57)'
+                className='js-color-stop'
+                style={{height: '100vh'}}
+            >
+                {/* content */}
+            </section>
+        </div>
+    )
 }
 
 export default App;
