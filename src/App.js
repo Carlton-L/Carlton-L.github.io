41ca24b88b904df635d2ff3aa104d844ccc37386
Carlton <Carlton.l@outlook.com>
2020-05-06 20:06:33 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,18 @@ const App = () => {
             <section
                 data-background-color='rgb(81, 24, 69)'
                 className='js-color-stop'
-                style={{height: '100vh'}}
             >
                 {/* content */}
             </section>
             <section
                 data-background-color='rgb(144, 12, 63)'
                 className='js-color-stop'
-                style={{height: '100vh'}}
             >
                 {/* content */}
             </section>
             <section
                 data-background-color='rgb(199, 0, 57)'
                 className='js-color-stop'
-                style={{height: '100vh'}}
             >
                 {/* content */}
             </section>
