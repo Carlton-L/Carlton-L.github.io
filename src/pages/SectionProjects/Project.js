6153e3834e3015528589605199dfd4e2ad4b7d2b
Carlton <Carlton.l@outlook.com>
2020-05-07 20:14:47 -0700

    Added components
    ContentContainer, PageContainer, Project, AnimatedLetters

---
+++ src/pages/SectionProjects/Project.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Project extends React.Component {
+    state={}
+
+    render() {
+        return (
+
+        ) 
+    }
+}
+
+export default Project;
--- src/pages/SectionProjects/index.js
+++ src/pages/SectionProjects/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PageContainer from '../PageContainer';
+import ContentContainer from '../../components/ContentContainer';
 
 class SectionProjects extends React.Component {
     state = {};
 
     render() {
         return (
-            <h2>Contact</h2>
+            <PageContainer>
+                <ContentContainer>
+                    <h2>Projects</h2>
+                </ContentContainer>
+                <ContentContainer h="center">
+                    <ContentContainer h="center">
+
+                    </ContentContainer>
+                </ContentContainer>
+            </PageContainer>
         )
     }
 }
