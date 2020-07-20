1e9c9b00f6c2ecf27a5753f6e010788a6a89f35e
Carlton <Carlton.l@outlook.com>
2020-07-20 14:10:51 -0700

    Updated to simple format with text carousel

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import React from 'react';
 import './App.css';
 import SectionHeader from './pages/SectionHeader';
-import SectionProjects from './pages/SectionProjects';
-import SectionContact from './pages/SectionContact';
-
-
+import SectionPortfolio from './pages/SectionPortfolio';
 
 const App = () => {
     return (
         <div>
-            <section
-                data-background-color='rgb(81, 24, 69)'
-                className='js-color-stop'
-            >
-                <SectionHeader />
-            </section>
-            <section data-background-color='rgb(144, 12, 63)' className='js-color-stop color-stop-filler'></section>
-            <section
-                data-background-color='rgb(144, 12, 63)'
-                className='js-color-stop'
-            >
-                <SectionProjects />
-            </section>
-            <section data-background-color='rgb(144, 12, 63)' className='js-color-stop color-stop-filler'></section>
-            <section
-                data-background-color='rgb(199, 0, 57)'
-                className='js-color-stop'
-            >
-                <SectionContact />
-            </section>
+            <SectionHeader />
+
+            <SectionPortfolio />
         </div>
-    )
-}
+    );
+};
 
 export default App;
---
+++ src/pages/SectionPortfolio/SectionPortfolio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SectionPortfolio = () => {
+    return (
+        <div className="section-container panel" data-color="gray">
+            <div className="container">
+                <h2>Portfolio</h2>
+                <p>{'{ currently empty - check back later :) }'}</p>
+            </div>
+        </div>
+    );
+};
+
+export default SectionPortfolio;
