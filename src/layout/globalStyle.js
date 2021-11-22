d4fea26a115172be9b8f5eac1af95db3f48a4dfa
Carlton Lindsay <carlton.l@outlook.com>
2021-11-21 23:41:56 -0800

    Create globalStyle.js

---
+++ src/layout/globalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import MenloRegular from '../fonts/Menlo-Regular.ttf';
+import MenloBold from '../fonts/Menlo-Bold.ttf';
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+@font-face {
+  font-family: Menlo-Regular;
+  src: local('Menlo-Regular'),
+  url(${MenloRegular});
+}
+@font-face {
+  font-family: Menlo-Bold;
+  src: local('Menlo-Bold'),
+  url(${MenloBold});
+}
+html, body {
+  font-family: Menlo, Menlo-Regular, Monaco, 'Courier New', monospace;
+  box-sizing: border-box
+}
+`;
+
+export default GlobalStyle;
