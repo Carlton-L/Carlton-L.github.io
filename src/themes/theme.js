3e86c2cb16c1de31fad9a5bf8221a25a2830ac12
Carlton Lindsay <carlton.l@outlook.com>
2021-11-22 00:03:14 -0800

    Renamed theme file

---
+++ src/themes/theme.js
@@ -0,0 +1,63 @@
+const theme = {
+  colors: {
+    background: '#282828',
+    paper: '#3C3836',
+    primary: '#F44747',
+    secondary: '#689D6A',
+    tertiary: '#D3869B',
+    error: '#FB4934',
+    warning: '#FABD2F',
+    info: '#6796E6',
+    inactive: '#928374',
+    textPrimary: '#EBDBB2',
+    textSecondary: '#83A598',
+    modes: {
+      dark: {
+        background: '#282828',
+        paper: '#3C3836',
+        primary: '#F44747',
+        secondary: '#689D6A',
+        tertiary: '#D3869B',
+        error: '#FB4934',
+        warning: '#FABD2F',
+        info: '#6796E6',
+        inactive: '#928374',
+        textPrimary: '#EBDBB2',
+        textSecondary: '#83A598',
+      },
+      light: {
+        background: '#EBDBB2',
+        paper: '#FBF1C7',
+        primary: '#CD3131',
+        secondary: '#A89984',
+        tertiary: '#8F3F71',
+        error: '#9D0006',
+        warning: '#B57614',
+        info: '#316BCD',
+        inactive: '#928374',
+        textPrimary: '#3C3836',
+        textSecondary: '#076678',
+      }
+    }
+  }
+};
+
+export default theme;
+
+/*
+const theme = {
+  colors: {
+    background: "",
+    paper: "",
+    primary: "",
+    secondary: "",
+    tertiary: "",
+    error: "",
+    warning; "",
+    info: "",
+    inactive: "",
+    textPrimary: "",
+    textSecondary: "",
+  }
+}
+*/
