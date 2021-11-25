f2d05666450865825ca1aae15cdedbb66da1006b
Carlton Lindsay <carlton.l@outlook.com>
2021-11-24 21:44:25 -0800

    Update themeSwitch.js

--- src/components/themeSwitch.js
+++ src/components/themeSwitch.js
@@ -6,13 +6,15 @@ import setLocalStorage from '../hooks/setLocalStorage';
 import Button from './button';
 
 const getNextTheme = (currentTheme, themeOptions) => {
+  // Find the index of the current theme, add 1 to find the next index
   const nextIndex = themeOptions.indexOf(currentTheme) + 1;
+  // If the next index value is longer than the length of the array, re-start the count
   return nextIndex >= themeOptions.length ? themeOptions[0] : themeOptions[nextIndex];
 };
 
 const ThemeSwitch = ({ currentTheme, themeOptions, handleTheme }) => {
   const [nextTheme, setNextTheme] = useState(getNextTheme(currentTheme, themeOptions));
-  setLocalStorage(nextTheme);
+  // setLocalStorage(nextTheme);
   return (
     <Button
       css={`
@@ -23,8 +25,11 @@ const ThemeSwitch = ({ currentTheme, themeOptions, handleTheme }) => {
       color={baseTheme.colors.modes[nextTheme].textPrimary}
       backgroundColor={baseTheme.colors.modes[nextTheme].paper}
       onClick={() => {
+        // Set the theme in state of MainLayout
         handleTheme(nextTheme);
-        // setLocalStorage(nextTheme);
+        // Set the theme in localStorage
+        setLocalStorage('theme', nextTheme);
+        // Update the button with the next theme in the queue
         setNextTheme(getNextTheme(nextTheme, themeOptions));
       }}
       variant="small"
