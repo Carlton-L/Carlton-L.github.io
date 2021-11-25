805f8e99d41b4e55c443786e886470861049496d
Carlton Lindsay <carlton.l@outlook.com>
2021-11-24 16:53:40 -0800

    Create footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Social from './social';
+import Email from './email';
+import { email, socialLinks } from '../utils/config';
+
+const Wrapper = styled.footer`
+  // TODO: Make height a var
+  height: 400px;
+  width: 100vw;
+  position: fixed;
+  bottom: 0;
+
+  z-index: 150;
+
+  display: flex;
+  justify-content: center;
+  `;
+
+const Content = styled.div`
+  display: none;
+  max-width: 1200px;
+  flex-grow: 1;
+  justify-content: space-between;
+  align-items: flex-end;
+
+  @media (min-width: 1200px) {
+    display: flex;
+  } 
+`;
+
+const Footer = () => {
+  const variable = [];
+  return (
+    <Wrapper>
+      <Content>
+        <Social socialLinks={socialLinks} />
+        <Email email={email} />
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Footer;
