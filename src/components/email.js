6be69d5a371ea930ca717f98950d903c1b12af41
Carlton Lindsay <carlton.l@outlook.com>
2021-11-24 16:53:38 -0800

    Create email.js

---
+++ src/components/email.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { color } from 'styled-system';
+
+const Wrapper = styled.div`
+  ${color}
+  width: 40px;
+
+  z-index: 150;
+  display: none;
+
+  @media (min-width: 834px) {
+    display: flex;
+  }
+`;
+
+const Content = styled.div`
+  ${color}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  background-clip: text;
+
+  &:after {
+    // TODO: Make background-color a var tied to color of parent
+    background-color: inherit;
+    background-clip: border-box;
+    content: '';
+    display: block;
+    width: 2px;
+    height: 90px;
+    margin: 0 auto;
+  }
+
+  a {
+    z-index: 100;
+    font-size: 0.75rem;
+    color: inherit;
+    margin: 0px auto;
+    padding: 10px;
+    writing-mode: vertical-rl;
+    text-decoration: none;
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+      transform: translateY(-3px);
+    }
+`;
+
+const Email = ({ email }) => {
+  const thing = [];
+  return (
+    <Wrapper>
+      <Content color="inactive" backgroundColor="inactive">
+        <a href={`mailto:${email}`}>{email}</a>
+      </Content>
+    </Wrapper>
+  );
+};
+
+Email.propTypes = {
+  email: PropTypes.string,
+};
+
+Email.defaultProps = {
+  email: 'email@email.com',
+};
+
+export default Email;
