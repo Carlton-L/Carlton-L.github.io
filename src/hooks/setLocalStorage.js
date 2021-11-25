514bd3db0669183e958c060d5043924c0cb8ab5a
Carlton Lindsay <carlton.l@outlook.com>
2021-11-25 11:00:47 -0800

    Update setLocalStorage.js

--- src/hooks/setLocalStorage.js
+++ src/hooks/setLocalStorage.js
@@ -1,5 +1,7 @@
 const setLocalStorage = (key, value) => {
-  window.localStorage.setItem(key, value);
+  if (typeof window !== 'undefined') {
+    window.localStorage.setItem(key, value);
+  }
 };
 
 export default setLocalStorage;
