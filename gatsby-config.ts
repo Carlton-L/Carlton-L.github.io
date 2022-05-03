b8e1a6a2195000d02f512a23f7f21d4273317112
Carlton Lindsay <carlton.l@outlook.com>
2022-05-03 15:38:06 -0700

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,27 +3,37 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Carlton.dev`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-sanity',
-    options: {
-      "projectId": "wc7chx9j",
-      "dataset": ""
-    }
-  }, "gatsby-plugin-theme-ui", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "G-RBGYMB3WPS"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    // TODO: Setup Sanity.io
+    // {
+    //   resolve: "gatsby-source-sanity",
+    //   options: {
+    //     projectId: "wc7chx9j",
+    //     dataset: "production",
+    //   },
+    // },
+    "gatsby-plugin-theme-ui",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-RBGYMB3WPS",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+        __key: "images",
+      },
+    },
+  ],
 };
 
 export default config;
